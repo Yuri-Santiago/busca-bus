f25c82758940def2e3e2f80786e7e1efecb2644d
Yuri Santiago <yurimateussantiago@gmail.com>
2021-12-15 11:34:12 -0300

    modificando porta

--- api.js
+++ api.js
@@ -35,6 +35,11 @@ app.get("/linhas/:search", function(req, res) {
   });
 });
 
-app.listen(9001, '0.0.0.0', function() {
-  console.log('Listening on port 9001');
+// app.listen(9001, '0.0.0.0', function() {
+//   console.log('Listening on port 9001');
+// });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
